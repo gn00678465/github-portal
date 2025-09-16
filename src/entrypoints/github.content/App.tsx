0abfd7da2b4852ca1b0a5e09a8f284a02d2ce782
Madao <gn00678465@gmail.com>
2025-09-16 08:51:27 +0800

    refactor: 將下拉選單改為按鈕以簡化介面
    - 移除 Dropdown menu 相關檔案與 package

--- src/entrypoints/github.content/App.tsx
+++ src/entrypoints/github.content/App.tsx
@@ -1,18 +1,9 @@
-import { ExternalLink } from 'lucide-react'
 import { Button } from '@/components/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
 import DeepWikiIcon from './DeepWiki'
 import GitIngestIcon from './GitIngest'
 
 function App() {
-  function onSelect(method: 'deepwiki' | 'gitingest') {
+  function onClick(method: 'deepwiki' | 'gitingest') {
     switch (method) {
       case 'deepwiki':
         browser.runtime.sendMessage({ action: 'redirect:deepwiki', url: window.location.href })
@@ -30,29 +21,17 @@ function App() {
   }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button className="h-[28px] text-sm p-[3px_12px]" variant="outline">
-          <ExternalLink />
-          {browser.i18n.getMessage('trigger')}
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent side="bottom" align="start" className="w-[--radix-dropdown-menu-trigger-width]">
-        <DropdownMenuGroup>
-          <DropdownMenuItem onSelect={() => onSelect('deepwiki')}>
-            <DeepWikiIcon />
-            {browser.i18n.getMessage('open_in_deepwiki')}
-          </DropdownMenuItem>
+    <div className="flex items-center gap-x-2">
+      <Button className="h-[28px] text-xs p-[3px_12px] cursor-pointer" variant="outline" onClick={() => onClick('deepwiki')}>
+        <DeepWikiIcon />
+        {browser.i18n.getMessage('open_in_deepwiki')}
+      </Button>
 
-          <DropdownMenuSeparator />
-
-          <DropdownMenuItem onSelect={() => onSelect('gitingest')}>
-            <GitIngestIcon />
-            {browser.i18n.getMessage('open_in_gitingest')}
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
+      <Button className="h-[28px] text-xs p-[3px_12px] cursor-pointer" variant="outline" onClick={() => onClick('gitingest')}>
+        <GitIngestIcon />
+        {browser.i18n.getMessage('open_in_gitingest')}
+      </Button>
+    </div>
   )
 }
 
